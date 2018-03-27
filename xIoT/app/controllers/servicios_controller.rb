7827c06017c5ec72976d67d8dfed72e97ae7836c
lacruz87 <luchocruz@gmail.com>
2018-03-27 19:32:49 -0300

    aa

--- xIoT/app/controllers/servicios_controller.rb
+++ xIoT/app/controllers/servicios_controller.rb
@@ -31,14 +31,38 @@ class ServiciosController < ApplicationController
   	@cuenta=EventIn.sum('valor') 
 
   	@prods=EventIn.order('published_at_dt')
-  	max_val=EventIn.maximum('valor')
+    max_val=1
+    begin
+    	max_val=EventIn.maximum('valor')
+    rescue
+      max_val=1
+    end
+
+    if(max_val==nil)
+      max_val=10
+    end
+
   	max_val=max_val*1.05
   	#@prod_json=@prod.to_json( :only => [:published_at_dt, :valor] )
   	@arr= Array.new 
+    @arr2= Array.new 
+
   	@arr.push(['Hora','Unidades','Capacidad'])
+
+    #@VAL1="{role: 'style' }"
+    #@arr2.push(['Hora', 'Cantidad', @VAL1 ])
+
+    val_acumulado=0
+
   	@prods.each do |prod|
   		val2=max_val-prod.valor
   		@arr.push([prod.published_at_dt.strftime("%H:%M:%S"),prod.valor,val2])
+
+      val_acumulado=val_acumulado+prod.valor
+      #[prod.published_at_dt.strftime("%H:%M:%S")
+      @arr2.push( [prod.published_at_dt.strftime("%H:%M:%S"), val_acumulado,prod.valor])
+
+
   	end
 
 
